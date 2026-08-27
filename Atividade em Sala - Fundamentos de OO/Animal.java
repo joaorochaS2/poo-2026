6443e1bb28d7f50c5f1d7c7e709cbcc8b9644dc6
João Lucas Rocha de Sousa Pereira <joao.rocha2@estudante.ifgoiano.edu.br>
2026-08-27 08:08:44 -0300

    Create Animal class with attributes and methods
    Classe Animal com atributos e métodos

---
+++ Atividade em Sala - Fundamentos de OO/Animal.java
@@ -0,0 +1,67 @@
+public class Animal{
+    private String nome;
+    private String especie;
+    private int idade;
+    private Dono dono;
+
+    public Animal(){
+        setNome("Sem Nome");
+        setEspecie("Não informado");
+        setIdade(0);
+        setDono(null);
+    }
+    public Animal(String n, String e, int i, Dono d){
+        setNome(n);
+        setEspecie(e);
+        setIdade(i);
+        setDono(d);
+    }
+
+    public void setNome(String n){
+        if (n!=""){
+            nome = n;
+        }
+        else{
+            System.out.println("Nome inválido.");
+        }
+    }
+    public void setEspecie(String e){
+        especie = e;
+    }
+    public void setIdade(int i){
+        if (i>=0){
+            idade = i;
+        }
+        else{
+            System.out.println("Idade inválida.");
+            setNome("Sem nome");
+        }
+    }
+    public void setDono(Dono d){
+        dono = d;
+    }
+    
+
+    public String getNome(){
+        return nome;
+    }
+    public String getEspecie(){
+        return especie;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public Dono getDono(){
+        return dono;
+    }
+
+    public void exibir(){
+        System.out.print("\nDados do Animal \nNome: " + nome + "\nEspecie: " + especie + "\nIdade: " + idade + "\nDono: ");
+        if (dono == null){
+            System.out.println("Sem dono");
+        }
+        else{
+            System.out.println(dono.getNome());
+        }
+    }
+}
